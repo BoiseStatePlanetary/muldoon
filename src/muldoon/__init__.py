2bdbaba3b6a35cf4141c9a8f93c8601897eb0cd6
Brian Jackson <bjackson@boisestate.edu>
2021-08-17 14:58:07 -0600

    Began adding required package elements

---
+++ src/muldoon/__init__.py
@@ -0,0 +1,8 @@
+"""A package for sifting meteorological data to look for and analyze vortex
+encounters"""
+
+__version__ = '0.0.0'
+__author__ = 'Brian Jackson <bjackson@boisestate.edu>'
+__all__ = ['muldoon']
+
+from .muldoon import *

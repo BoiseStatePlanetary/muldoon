989e63a36720291e99dcfc7274163f798bf94c23
Brian Jackson <decaelus@gmail.com>
2021-08-17 19:28:57 -0600

    Added first function

--- src/muldoon/muldoon.py
+++ src/muldoon/muldoon.py
@@ -1,2 +1,22 @@
-def add_one(number):
-    return number + 1
+def __init__(time, pressure, windspeed=None, wind_direction=None):
+    """
+    Process and analyze a meteorological time-series to search for vortices
+
+    Args:
+        time (float, array): time of meteorological time-series
+        pressure (float, array): pressure measurements
+        windspeed (float, array, optional): wind speed measurements
+        wind_direction (float, array, optional): wind velocity aziumth
+
+    Returns:
+        meteorological time-series object
+    """
+
+    self.time = time
+    self.pressure = pressure
+
+    if(windspeed is not None):
+        self.windspeed = windspeed
+
+    if(wind_direction is not None):
+        self.wind_direction = wind_direction

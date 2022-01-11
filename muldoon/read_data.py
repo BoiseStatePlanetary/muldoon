44dd366dd95266506e1629b0b3a0b1bf7f976d90
Brian Jackson <bjackson@boisestate.edu>
2022-01-10 17:21:33 -0700

    Added the ability to return LMST times as well as LTST times

--- muldoon/read_data.py
+++ muldoon/read_data.py
@@ -5,7 +5,7 @@ A collections of routines to read in data from various missions
 import numpy as np
 import pandas as pd
 
-def read_Perseverance_PS_data(filename, sol=None):
+def read_Perseverance_PS_data(filename, sol=None, time_field='LTST'):
     """
     Read in Perseverance MEDA PS data - https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/
 
@@ -17,18 +17,20 @@ def read_Perseverance_PS_data(filename, sol=None):
         since midnight of sol associated with filename
     """
 
-    time = make_seconds_since_midnight(filename)
+    time = make_seconds_since_midnight(filename, time_field=time_field)
     pressure = pd.read_csv(filename)['PRESSURE'].values
 
     return time, pressure
 
-def read_Perseverance_ATS_data(filename, which_ATS=1, sol=None):
+def read_Perseverance_ATS_data(filename, which_ATS=1, time_field='LTST', 
+        sol=None):
     """
     Read in Perseverance MEDA ATS data - https://pds-atmospheres.nmsu.edu/PDS/data/PDS4/Mars2020/mars2020_meda/
 
     Args:
         filename (str): path to CSV file
         which_ATS (int): which of the five ATS sensors to read in
+        time_field (str, optional): which time base to use
 
     Returns:
         time, pressure (float array): times and pressures, times in seconds
@@ -37,7 +39,7 @@ def read_Perseverance_ATS_data(filename, which_ATS=1, sol=None):
     """
 
     # Note: ATS measures at 2 Hz, so there will be some duplicate LTST-values!
-    time = make_seconds_since_midnight(filename)
+    time = make_seconds_since_midnight(filename, time_field=time_field)
 
     # Which ATS time-series to read in?
     which_ATS_str = "ATS_LOCAL_TEMP%i" % which_ATS
@@ -45,12 +47,13 @@ def read_Perseverance_ATS_data(filename, which_ATS=1, sol=None):
 
     return time, temperature
 
-def make_seconds_since_midnight(filename, sol=None):
+def make_seconds_since_midnight(filename, time_field='LTST', sol=None):
     """
     The MEDA data provide times in the LTST field in the format "sol hour:minute:second".
 
     Args:
         filename (str): name of the file
+        time_field (str, optional): name of time field to analyze
         sol (int, optional): which is the primary sol; if not given, will
         determine from filename
 
@@ -63,17 +66,25 @@ def make_seconds_since_midnight(filename, sol=None):
     data = pd.read_csv(filename)
 
     # Grab the sols and times associated with each row
-    sols_str = data['LTST'].str.split(expand=True)[0].values
-    times_str = data['LTST'].str.split(expand=True)[1].values
+    if(time_field == "LTST"):
+        sols_str = data[time_field].str.split(expand=True)[0].values
+        times_str = data[time_field].str.split(expand=True)[1].values
+        # Turn the times strings into seconds since midnight of the primary sol
+        delta_sols = sols_str.astype(float) - float(primary_sol)
 
-    # Turn the times strings into seconds since midnight of the primary sol
-    delta_sols = sols_str.astype(float) - float(primary_sol)
+        # And then in a very cludgey way, convert times to seconds since primary sol's midnight
+        time = np.array([float(times_str[i].split(":")[0]) +\
+                delta_sols[i]*24. + float(times_str[i].split(":")[1])/60 +\
+                float(times_str[i].split(":")[2])/3600. for i in
+                range(len(times_str))])
 
-    # And then in a very cludgey way, convert times to seconds since primary sol's midnight
-    time = np.array([float(times_str[i].split(":")[0]) + delta_sols[i]*24.  +\
-            float(times_str[i].split(":")[1])/60 +\
-            float(times_str[i].split(":")[2])/3600. for i in
-            range(len(times_str))])
+    elif(time_field == "LMST"):
+        times_str = data[time_field].str.split("M", expand=True)[1].values
+
+        time = np.array([float(times_str[i].split(":")[0]) +\
+                float(times_str[i].split(":")[1])/60 +\
+                float(times_str[i].split(":")[2])/3600. for i in
+                range(len(times_str))])
 
     return time
 
